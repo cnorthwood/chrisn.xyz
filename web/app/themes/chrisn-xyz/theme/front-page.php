d4c85aa2fdca9878c820de830a551136b7cdf9a4
Chris Northwood <chris@pling.org.uk>
2023-10-11 12:09:50 +0100

    start laying out homepage

--- web/app/themes/chrisn-xyz/theme/front-page.php
+++ web/app/themes/chrisn-xyz/theme/front-page.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
-  <main class="cn-content-box">
-    <h1 class="t-arndale">TODO</h1>
+  <main>
+    <header class="cn-content-box cn-home-info">
+      <h1 class="t-arndale">
+          <span><?php echo get_bloginfo("name"); ?></span>
+          <img src="<?php header_image(); ?>" width="124" height="124" class="cn-home-info__profile" alt="A photograph of Chris Northwood">
+      </h1>
+      <p class="t-body"><?php echo get_bloginfo("description", "display") ?></p>
+    </header>
+    <?php wp_nav_menu(["theme_location" => "front-page"]); ?>
   </main>
 <?php get_footer();
--- web/app/themes/chrisn-xyz/theme/functions.php
+++ web/app/themes/chrisn-xyz/theme/functions.php
@@ -5,6 +5,7 @@ function chrisnxyz_setup()
     add_theme_support('automatic-feed-links');
     add_theme_support('html5', ['script', 'style']);
     add_theme_support('title-tag');
+    add_theme_support('custom-header', ["width" => 248, "height" => 248]);
 }
 add_action('after_setup_theme', 'chrisnxyz_setup');
 
@@ -13,3 +14,9 @@ function chrisnxyz_enqueue_scripts()
     vite_load_asset("chrisnxyz_main", "assets/main.ts");
 }
 add_action('wp_enqueue_scripts', 'chrisnxyz_enqueue_scripts');
+
+function register_my_menus()
+{
+    register_nav_menus(["front-page" => "Front page"]);
+}
+add_action("init", "register_my_menus");
